7c64af6e2966238c03e5fda92f41cd0985798f32
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-13 03:10:23 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -25,7 +25,7 @@ class DBStorage:
 
     __engine = None
     __session = None
-   
+
     def __init__(self):
         """Initializes a new DBStorage instance."""
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
@@ -36,7 +36,7 @@ class DBStorage:
                                       pool_pre_ping=True)
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
-        
+
     def all(self, cls=None):
         """Query on the curret database session all objects of the given class.
         If cls is None, queries all types of objects.
