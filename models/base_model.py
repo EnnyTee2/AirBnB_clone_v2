4865a304016ff93bd5b3e9bc33a84299c13ac630
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-13 03:08:55 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
