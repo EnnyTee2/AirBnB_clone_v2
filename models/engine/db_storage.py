50f75695cab61f7fa1e1fea2f68d265aa66260ad
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-13 02:32:51 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -50,11 +50,11 @@ class DBStorage:
             obj = self.__session.query(State)
         else:
             obj = self.__session.query(State)
-            obj.extend(self.__session.query(City)
-            obj.extend(self.__session.query(User)
-            obj.extend(self.__session.query(Place)
-            obj.extend(self.__session.query(Review)
-            obj.extend(self.__session.query(Amenity)
+            obj.extend(self.__session.query(City).all())
+            obj.extend(self.__session.query(User).all())
+            obj.extend(self.__session.query(Place).all())
+            obj.extend(self.__session.query(Review).all())
+            obj.extend(self.__session.query(Amenity).all())
         obj_dico = {"{}.{}".format(type(x).__name__, x.id): x for o in obj}
 
         return obj_dico
