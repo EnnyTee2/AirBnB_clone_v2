a412153b47c27796a4fecaa3508274a65a1f8c01
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-28 04:27:38 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,18 @@
 #!/usr/bin/env bash
-# Bash script that sets up the web servers for the deployment of web_static
-
-# Install nginx if not installed
-if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-  apt update
-  apt install -y nginx;
-fi
-
-# making required directories
-sudo mkdir -p /data;
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-
-# Dummy HTML file to test server config
-printf %s "<html>
-   <head>
-   </head>
-   <body>
-     Holberton School
-   </body>
- </html>
- " | sudo tee /data/web_static/releases/test/index.html
-
-# Create the specified symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Giving ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -R ubuntu:ubuntu /data
-
-# Update the Nginx configuration file to serve the specified content
+# Setup a web servers for the deployment of web_static.
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-
-sudo service nginx reload
 sudo service nginx restart
