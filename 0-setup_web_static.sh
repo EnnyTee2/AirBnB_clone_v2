597f5a1996a06fef0d255ad9835075015ee995f6
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-28 04:47:38 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,45 @@
-#!/usr/bin/env bash
 # Bash script that sets up the web servers for the deployment of web_static
 
-
+# Install nginx if not installed
 apt update
-apt install -y nginx
+apt install -y nginx;
 
 # making required directories
-mkdir -p /data;
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data;
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
 # Dummy HTML file to test server config
-printf %s "<!DOCTYPE html>
-<html>
+printf %s "<html>
    <head>
    </head>
    <body>
      Holberton School
    </body>
- </html>" | tee /data/web_static/releases/test/index.html
+ </html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create the specified symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 # Giving ownership of the /data/ folder to the ubuntu user AND group
-chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration file to serve the specified content
-sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo chmod 755 /etc/nginx/sites-available/default
+printf %s "server {
+  listen 80;
+  listen [::]:80;
 
+  server_name _;
+  root /data/web_static/current;
+  index index.html;
+
+  location /hbnb_static {
+    alias /data/web_static/current/;
+  }
+}" > /etc/nginx/sites-available/default
+
+sudo service nginx reload
 sudo service nginx restart
