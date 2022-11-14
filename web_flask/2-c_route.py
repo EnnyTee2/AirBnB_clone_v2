798288b9878569471222084a7ec38df994f31bed
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-14 13:10:02 +0100

    Create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Starts a simple Flask web app.
+    The app listens on 0.0.0.0, port 5000.
+    Routes:
+        /: Displays 'Hello HBNB!'
+        /hbnb: Displays 'HBNB'
+        /c/<text>: Displays 'C <text>'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello(strict_slashes=False):
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hello_bnb(strict_slashes=False):
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def hello_text(strict_slashes=False):
+    """ Displays 
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
