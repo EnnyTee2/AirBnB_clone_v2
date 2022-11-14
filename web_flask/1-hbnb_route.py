defb0f2868a20d49af45c1bf4a77fb5b454e9561
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-14 12:54:43 +0100

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,6 +15,7 @@ def hello(strict_slashes=False):
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hello_bnb(strict_slashes=False):
     """Displays 'HBNB'"""
