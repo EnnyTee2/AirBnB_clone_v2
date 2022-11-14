d5d1403261860eddd9d74c63403040c6b37e6ba5
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-11-14 14:31:03 +0100

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -39,7 +39,7 @@ def hello_python(text='is cool'):
 
 
 @app.route('/number/<int: n>', strict_slashes=False)
-def hello_c(n, strict_slashes=False):
+def hello_number(n, strict_slashes=False):
     """ Displays 'n is a number' only if n is an integer"""
     return "{} is a number".format(n)
 
